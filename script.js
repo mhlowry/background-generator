cbe42561384a90140cc06d4e315b7a33ca43803e
Michael Lowry <michael.lowry64@gmail.com>
2024-05-03 13:44:21 -0400

    linear gradient information appears on screen

--- script.js
+++ script.js
@@ -1,17 +1,16 @@
 let button1 = document.querySelector("#color1");
 let button2 = document.querySelector("#color2");
 let body = document.querySelector("body");
+let h3 = document.querySelector("h3"); 
 
 function setNewBackground() {
 	body.style.background = "linear-gradient(to right, "
 	+ button1.value + ", "
 	+ button2.value + ")";
+
+	h3.innerText = body.style.background + ";";
 }
 
-button1.addEventListener("change", function() {
-	setNewBackground();
-})
+button1.addEventListener("change", setNewBackground);
 
-button2.addEventListener("change", function() {
-	setNewBackground(); 
-})
+button2.addEventListener("change", setNewBackground);
